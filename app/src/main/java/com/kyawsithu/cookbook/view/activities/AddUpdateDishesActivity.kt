a5313ee18bcd9dcef14574f8baf0c77ad5fe8185
KST PC <kyawcthumoeoo@gmail.com>
2022-03-15 14:55:14 +0630

    Added: AddUpdateDishesActivity

---
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
@@ -0,0 +1,12 @@
+package com.kyawsithu.cookbook.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kyawsithu.cookbook.R
+
+class AddUpdateDishesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update)
+    }
+}
