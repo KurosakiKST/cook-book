42b6c6e26121acbeb5eeb092a308076848833a25
KST PC <kyawcthumoeoo@gmail.com>
2022-03-15 22:39:50 +0630

    Added: Custom Dialog for image selection

--- app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
@@ -1,11 +1,13 @@
 package com.kyawsithu.cookbook.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.databinding.ActivityAddUpdateDishesBinding
+import com.kyawsithu.cookbook.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var addUpdateDishesBinding: ActivityAddUpdateDishesBinding
@@ -20,7 +22,7 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    private fun setupActionBar(){
+    private fun setupActionBar() {
         setSupportActionBar(addUpdateDishesBinding.toolbarAddUpdateDishActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         addUpdateDishesBinding.toolbarAddUpdateDishActivity.setNavigationOnClickListener {
@@ -29,13 +31,32 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        if(view != null){
-            when(view.id){
+        if (view != null) {
+            when (view.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this, "Image View Add", Toast.LENGTH_SHORT).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
