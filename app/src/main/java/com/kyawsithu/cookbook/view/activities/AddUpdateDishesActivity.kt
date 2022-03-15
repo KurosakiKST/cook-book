34365f56692952ae94ac5864929803d4e6afbc32
KST PC <kyawcthumoeoo@gmail.com>
2022-03-15 21:37:15 +0630

    Added: Back button in AddUpdateDishesActivity

--- app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
@@ -3,10 +3,25 @@ package com.kyawsithu.cookbook.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kyawsithu.cookbook.R
+import com.kyawsithu.cookbook.databinding.ActivityAddUpdateDishesBinding
 
 class AddUpdateDishesActivity : AppCompatActivity() {
+    private lateinit var addUpdateDishesBinding: ActivityAddUpdateDishesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dishes)
+        addUpdateDishesBinding = ActivityAddUpdateDishesBinding.inflate(layoutInflater)
+        setContentView(addUpdateDishesBinding.root)
+
+        setupActionBar()
+
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(addUpdateDishesBinding.toolbarAddUpdateDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        addUpdateDishesBinding.toolbarAddUpdateDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
