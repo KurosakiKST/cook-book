6cb2071531fa108442dc5fd6c76e8f2d10737bc5
KST PC <kyawcthumoeoo@gmail.com>
2022-03-15 14:15:11 +0630

    Updated: MVVM setup

--- app/src/main/java/com/kyawsithu/cookbook/view/activities/MainActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kyawsithu.cookbook
+package com.kyawsithu.cookbook.view.activities
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/kyawsithu/cookbook/view/activities/SplashActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.kyawsithu.cookbook
+package com.kyawsithu.cookbook.view.activities
 
 import android.content.Intent
 import android.os.Build
@@ -10,6 +10,7 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
