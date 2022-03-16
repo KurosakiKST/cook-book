e4fb030f88918cdd1967676508dafc73f5b434cd
KST PC <kyawcthumoeoo@gmail.com>
2022-03-16 14:38:24 +0630

    Updated: AddUpdateDishesActivity Added: Camera and Gallery input

--- app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
@@ -5,9 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.karumi.dexter.Dexter
 import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.databinding.ActivityAddUpdateDishesBinding
 import com.kyawsithu.cookbook.databinding.DialogCustomImageSelectionBinding
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var addUpdateDishesBinding: ActivityAddUpdateDishesBinding
@@ -48,15 +67,113 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(cameraIntent, CAMERA)
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
 
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ).withListener(object : PermissionListener {
+
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    val galleryIntent = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                    startActivityForResult(galleryIntent, GALLERY)
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDishesActivity,
+                        "You have denied storage permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
 
         dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == CAMERA){
+                data?.extras?.let{
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
+                    addUpdateDishesBinding.ivDishImage.setImageBitmap(thumbnail)
+                    addUpdateDishesBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+                }
+            }
+            else if(requestCode == GALLERY){
+                data?.let{
+                    val selectedPhotoURI = data.data
+                    addUpdateDishesBinding.ivDishImage.setImageURI(selectedPhotoURI)
+                    addUpdateDishesBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+                }
+            }else if(requestCode == Activity.RESULT_CANCELED){
+                Log.e("Cancel", "Image choosing cancel")
+            }
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage(
+                "You haven't gave permissions required for this feature. " +
+                        "It can be enabled under Application Setting"
+            )
+            .setPositiveButton("Go to Setting") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    companion object {
+        private const val CAMERA = 1
+        private const val GALLERY = 2
+
+        private const val IMAGE_DIRECTORY = "CookingRecipe"
     }
 }
