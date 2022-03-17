61d3d9c6971fe8849223971af013a0a4c4d380c1
lolvOid <linhtet1998itachi@gmail.com>
2022-03-17 23:11:34 +0630

    Update: Image path

--- app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/activities/AddUpdateDishesActivity.kt
@@ -13,14 +13,23 @@ import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -28,9 +37,15 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var addUpdateDishesBinding: ActivityAddUpdateDishesBinding
+    private var mImagePath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -140,6 +155,10 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
                             .load(thumbnail)
                             .centerCrop()
                             .into(addUpdateDishesBinding.ivDishImage)
+
+                    mImagePath = saveImageToInternalStorage(thumbnail)
+                    Log.i("ImagePath", mImagePath)
+
                     addUpdateDishesBinding.ivAddDishImage.setImageDrawable(
                         ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
                 }
@@ -151,7 +170,29 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
                     Glide.with(this)
                             .load(selectedPhotoURI)
                             .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .listener(object : RequestListener<Drawable>{
+                                override fun onLoadFailed(e : GlideException?, model : Any?, target : Target<Drawable>?, isFirstResource : Boolean) : Boolean
+                                {
+                                    Log.e("TAG", "Error loading image", e)
+                                    return false
+                                }
+
+                                override fun onResourceReady(resource : Drawable?, model : Any?, target : Target<Drawable>?, dataSource : DataSource?, isFirstResource : Boolean) : Boolean
+                                {
+                                    resource?.let{
+                                        val bitmap: Bitmap = resource.toBitmap()
+                                        mImagePath = saveImageToInternalStorage(bitmap)
+                                        Log.i("ImagePath", mImagePath)
+                                    }
+                                    return false
+                                }
+
+                            })
                             .into(addUpdateDishesBinding.ivDishImage)
+
+
+
                     addUpdateDishesBinding.ivAddDishImage.setImageDrawable(
                         ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
                 }
@@ -179,6 +220,23 @@ class AddUpdateDishesActivity : AppCompatActivity(), View.OnClickListener {
             }.setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
+    }
+
+    private fun saveImageToInternalStorage(bitmap : Bitmap): String{
+        val wrapper = ContextWrapper(applicationContext)
+
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try{
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e:IOException){
+            e.printStackTrace()
+        }
+        return file.absolutePath
     }
 
     companion object {
