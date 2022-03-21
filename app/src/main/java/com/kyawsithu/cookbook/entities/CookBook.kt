e9a57317941553699cb9362bdf5f6fd9d1919848
KST PC <kyawcthumoeoo@gmail.com>
2022-03-21 15:26:29 +0630

    Updated: cookbook model

--- app/src/main/java/com/kyawsithu/cookbook/entities/CookBook.kt
+++ app/src/main/java/com/kyawsithu/cookbook/entities/CookBook.kt
@@ -2,13 +2,19 @@ package com.kyawsithu.cookbook.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "cook_book_table")
 data class CookBook(
         @ColumnInfo val image : String,
-        @ColumnInfo val imageSource : String,
+        @ColumnInfo(name = "image_source") val imageSource: String,
         @ColumnInfo val title : String,
         @ColumnInfo val type : String,
         @ColumnInfo val category : String,
         @ColumnInfo val ingredients : String,
+
+        @ColumnInfo(name = "cooking_time") val cookingTime: String,
+        @ColumnInfo(name = "instructions") val directionToCook: String,
+        @ColumnInfo(name = "favourite_dish") val favouriteDish: Boolean = false,
+        @PrimaryKey(autoGenerate = true) val id: Int = 0
                    )
