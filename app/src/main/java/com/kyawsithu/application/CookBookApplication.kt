732748656e2303abed6a01f7e5074076e210d77a
KST PC <kyawcthumoeoo@gmail.com>
2022-03-21 10:20:52 +0630

    Added: Application

---
+++ app/src/main/java/com/kyawsithu/application/CookBookApplication.kt
@@ -0,0 +1,14 @@
+package com.kyawsithu.application
+
+import android.app.Application
+import com.kyawsithu.cookbook.model.database.CookBookRepository
+import com.kyawsithu.cookbook.model.database.CookBookRoomDatabase
+
+class CookBookApplication : Application()
+{
+    private val database by lazy { CookBookRoomDatabase.getDatabase(this@CookBookApplication) }
+
+    val repository by lazy { CookBookRepository(database.cookBookDao()) }
+
+
+}
