02895344b638cb270dadba46c80c3897bb7216f3
lolvOid <linhtet1998itachi@gmail.com>
2022-03-25 13:02:45 +0630

    added: color palette to layout background in dish details

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
@@ -1,13 +1,20 @@
 package com.kyawsithu.cookbook.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.databinding.FragmentDishDetailsBinding
 import kotlinx.coroutines.flow.combine
@@ -16,7 +23,7 @@ import java.util.*
 
 class DishDetailsFragment : Fragment()
 {
-    private var binding: FragmentDishDetailsBinding? = null
+    private var binding : FragmentDishDetailsBinding? = null
 
     override fun onCreate(savedInstanceState : Bundle?)
     {
@@ -28,31 +35,64 @@ class DishDetailsFragment : Fragment()
     {
         binding = FragmentDishDetailsBinding.inflate(inflater, container, false)
 
-        return binding!!.root
+        return binding !!.root
     }
 
     override fun onViewCreated(view : View, savedInstanceState : Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
-        val args: DishDetailsFragmentArgs by navArgs()
+        val args : DishDetailsFragmentArgs by navArgs()
 
 
         args.let {
-            try{
+            try
+            {
                 Glide.with(requireActivity())
                         .load(it.dishDetails.image)
                         .centerCrop()
-                        .into(binding!!.ivDishImage)
-            }catch (e: IOException){
+                        .listener(object : RequestListener<Drawable>
+                        {
+                            override fun onLoadFailed(
+                                    e : GlideException?,
+                                    model : Any?,
+                                    target : Target<Drawable>?,
+                                    isFirstResource : Boolean) : Boolean
+                            {
+                                Log.e("ImageTAG", "Error loading image")
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                    resource : Drawable?,
+                                    model : Any?,
+                                    target : Target<Drawable>?,
+                                    dataSource : DataSource?,
+                                    isFirstResource : Boolean) : Boolean
+                            {
+                                resource.let{
+                                    Palette.from(resource!!.toBitmap()).generate(){
+                                            palette ->
+                                        val intColor = palette?.vibrantSwatch?.rgb?: 0
+                                        binding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                    }
+                                }
+                                return false
+                            }
+
+                        })
+                        .into(binding !!.ivDishImage)
+            }
+            catch (e : IOException)
+            {
                 e.printStackTrace()
             }
-            binding!!.tvTitle.text = it.dishDetails.title
-            binding!!.tvType.text = it.dishDetails.type.capitalize(Locale.ROOT)
-            binding!!.tvCategory.text = it.dishDetails.category
-            binding!!.tvIngredients.text = it.dishDetails.ingredients
-            binding!!.tvCookingDirection.text = it.dishDetails.directionToCook
-            binding!!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time,
-                                                              it.dishDetails.cookingTime)
+            binding !!.tvTitle.text = it.dishDetails.title
+            binding !!.tvType.text = it.dishDetails.type.capitalize(Locale.ROOT)
+            binding !!.tvCategory.text = it.dishDetails.category
+            binding !!.tvIngredients.text = it.dishDetails.ingredients
+            binding !!.tvCookingDirection.text = it.dishDetails.directionToCook
+            binding !!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time,
+                it.dishDetails.cookingTime)
         }
     }
 
