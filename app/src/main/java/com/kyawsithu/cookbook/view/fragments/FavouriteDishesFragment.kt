16f172f3b5c986b75186a9996e7e304eef5e3804
KST PC <kyawcthumoeoo@gmail.com>
2022-03-25 14:40:43 +0630

    Updated: Favourite dishes list UI

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/FavouriteDishesFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/FavouriteDishesFragment.kt
@@ -1,48 +1,40 @@
 package com.kyawsithu.cookbook.view.fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.kyawsithu.cookbook.application.CookBookApplication
 import com.kyawsithu.cookbook.databinding.FragmentFavouriteDishesBinding
+import com.kyawsithu.cookbook.view.adapters.CookBookAdapter
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModel
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModelFactory
 import com.kyawsithu.cookbook.viewmodel.DashboardViewModel
 
 class FavouriteDishesFragment : Fragment() {
 
-    private var _binding: FragmentFavouriteDishesBinding? = null
+    private var binding: FragmentFavouriteDishesBinding? = null
 
     private val mCookBookViewModel: CookBookViewModel by viewModels {
         CookBookViewModelFactory((requireActivity().application as CookBookApplication).repository)
     }
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        binding = FragmentFavouriteDishesBinding.inflate(inflater, container, false)
 
-        _binding = FragmentFavouriteDishesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding!!.root
     }
 
     override fun onViewCreated(view : View, savedInstanceState : Bundle?)
@@ -51,12 +43,20 @@ class FavouriteDishesFragment : Fragment() {
         mCookBookViewModel.favouriteDishes.observe(viewLifecycleOwner){
             dishes ->
             dishes.let{
+
+                binding!!.rvFavoriteDishesList.layoutManager =
+                        GridLayoutManager(requireActivity(),2)
+
+                val adapter = CookBookAdapter(this)
+                binding!!.rvFavoriteDishesList.adapter = adapter
+
                 if(it.isNotEmpty()){
-                    for(dish in it){
-                        Log.i("FavDish", "${dish.id} :: ${dish.title}")
-                    }
+                    binding!!.rvFavoriteDishesList.visibility = View.VISIBLE
+                    binding!!.tvNoFavoriteDishesAvailable.visibility = View.GONE
+                    adapter.dishesList(it)
                 }else{
-                    Log.i("FavDish", "Empty dish")
+                    binding!!.rvFavoriteDishesList.visibility = View.GONE
+                    binding!!.tvNoFavoriteDishesAvailable.visibility = View.VISIBLE
                 }
             }
         }
@@ -64,6 +64,6 @@ class FavouriteDishesFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
