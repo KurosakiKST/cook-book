b9842f167322539a078efcffc7d23fcb35aa5c07
lolvOid <linhtet1998itachi@gmail.com>
2022-03-25 12:32:17 +0630

    Update: Dish Detail UI

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
@@ -7,10 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.kyawsithu.cookbook.R
+import com.kyawsithu.cookbook.databinding.FragmentDishDetailsBinding
+import kotlinx.coroutines.flow.combine
+import java.io.IOException
+import java.util.*
 
 class DishDetailsFragment : Fragment()
 {
+    private var binding: FragmentDishDetailsBinding? = null
 
     override fun onCreate(savedInstanceState : Bundle?)
     {
@@ -20,17 +26,40 @@ class DishDetailsFragment : Fragment()
     override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?,
                               savedInstanceState : Bundle?) : View?
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        binding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+
+        return binding!!.root
     }
 
     override fun onViewCreated(view : View, savedInstanceState : Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        Log.i("DishTitle", args.dishDetails.title)
-        Log.i("DishType", args.dishDetails.type)
 
+
+        args.let {
+            try{
+                Glide.with(requireActivity())
+                        .load(it.dishDetails.image)
+                        .centerCrop()
+                        .into(binding!!.ivDishImage)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            binding!!.tvTitle.text = it.dishDetails.title
+            binding!!.tvType.text = it.dishDetails.type.capitalize(Locale.ROOT)
+            binding!!.tvCategory.text = it.dishDetails.category
+            binding!!.tvIngredients.text = it.dishDetails.ingredients
+            binding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+            binding!!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time,
+                                                              it.dishDetails.cookingTime)
+        }
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        binding = null
     }
 
 }
