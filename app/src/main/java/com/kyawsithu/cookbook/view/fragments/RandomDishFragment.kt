3792e13ac8c98d3913ebaf4bc515e30a3e86a325
KST PC <kyawcthumoeoo@gmail.com>
2022-03-26 20:11:10 +0630

    Added: View model to RandomDishFragment

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/RandomDishFragment.kt
@@ -1,6 +1,7 @@
 package com.kyawsithu.cookbook.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kyawsithu.cookbook.databinding.FragmentRandomDishBinding
 import com.kyawsithu.cookbook.viewmodel.NotificationsViewModel
+import com.kyawsithu.cookbook.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment()
 {
 
     private var binding : FragmentRandomDishBinding? = null
+
+    private lateinit var mRandomDishViewModel : RandomDishViewModel
 
     override fun onCreateView(
             inflater : LayoutInflater,
@@ -23,6 +27,40 @@ class RandomDishFragment : Fragment()
     {
         binding = FragmentRandomDishBinding.inflate(inflater, container, false)
         return binding !!.root
+    }
+
+    override fun onViewCreated(view : View, savedInstanceState : Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        mRandomDishViewModel = ViewModelProvider(this).get(RandomDishViewModel::class.java)
+
+        mRandomDishViewModel.getRandomRecipeFromAPI()
+
+        randomDishViewModelObserver()
+    }
+
+    private fun randomDishViewModelObserver()
+    {
+        mRandomDishViewModel.randomDishResponse.observe(viewLifecycleOwner
+                                                       ) { randomDishResponse ->
+            randomDishResponse?.let {
+                Log.i("Random Dish", "$randomDishResponse.recipes[0]")
+
+            }
+        }
+        mRandomDishViewModel.randomDishLoadingError.observe(viewLifecycleOwner
+                                                           ) { dataError ->
+            dataError?.let {
+                Log.i("Random Dish Error", "$dataError")
+            }
+        }
+
+        mRandomDishViewModel.loadRandomDish.observe(viewLifecycleOwner) { loadRandomDish ->
+            loadRandomDish?.let {
+                Log.i("Random dish loading", "$loadRandomDish")
+            }
+        }
+
     }
 
     override fun onDestroy()
--- app/src/main/java/com/kyawsithu/cookbook/viewmodel/RandomDishViewModel.kt
+++ app/src/main/java/com/kyawsithu/cookbook/viewmodel/RandomDishViewModel.kt
@@ -1,6 +1,7 @@
 package com.kyawsithu.cookbook.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.kyawsithu.cookbook.model.entities.RandomDish
 import com.kyawsithu.cookbook.model.network.RandomDishAPIService
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -8,7 +9,7 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.DisposableSingleObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class RandomDishViewModel
+class RandomDishViewModel: ViewModel()
 {
     private val randomRecipeAPIService = RandomDishAPIService()
 
