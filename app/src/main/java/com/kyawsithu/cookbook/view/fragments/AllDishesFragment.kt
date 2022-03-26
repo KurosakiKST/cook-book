eb64e28049ad7af1cb5559f8addcbc4b7878db93
KST PC <kyawcthumoeoo@gmail.com>
2022-03-26 09:52:36 +0630

    Added: Filter Menu

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/AllDishesFragment.kt
@@ -1,6 +1,7 @@
 package com.kyawsithu.cookbook.view.fragments
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
@@ -8,13 +9,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kyawsithu.cookbook.application.CookBookApplication
 import com.kyawsithu.cookbook.R
+import com.kyawsithu.cookbook.databinding.DialogCustomListBinding
 import com.kyawsithu.cookbook.databinding.FragmentAllDishesBinding
 import com.kyawsithu.cookbook.model.entities.CookBook
+import com.kyawsithu.cookbook.utils.Constants
 import com.kyawsithu.cookbook.view.activities.AddUpdateDishesActivity
 import com.kyawsithu.cookbook.view.activities.MainActivity
 import com.kyawsithu.cookbook.view.adapters.CookBookAdapter
+import com.kyawsithu.cookbook.view.adapters.CustomListItemAdapter
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModel
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModelFactory
 
@@ -100,6 +105,21 @@ class AllDishesFragment : Fragment()
         alertDialog.show()
     }
 
+    private fun filterDishesListDialog(){
+        val customListDialog = Dialog(requireActivity())
+        val binding: DialogCustomListBinding = DialogCustomListBinding.inflate(layoutInflater)
+
+        customListDialog.setContentView(binding.root)
+        binding.tvTitle.text = resources.getString(R.string.title_select_item_to_filter)
+        val dishTypes = Constants.dishType()
+        dishTypes.add(0, Constants.ALL_ITEMS)
+
+        binding.rvList.layoutManager = LinearLayoutManager(requireActivity())
+        val adapter = CustomListItemAdapter(requireActivity(), dishTypes, Constants.FILTER_SELECTION)
+        binding.rvList.adapter = adapter
+        customListDialog.show()
+    }
+
     override fun onResume()
     {
         super.onResume()
@@ -122,6 +142,10 @@ class AllDishesFragment : Fragment()
             R.id.action_add_dish ->
             {
                 startActivity(Intent(requireActivity(), AddUpdateDishesActivity::class.java))
+                return true
+            }
+            R.id.action_filter_dishes ->{
+                filterDishesListDialog()
                 return true
             }
         }
