be6d1f7ec9def32d8086f7ebc099b7c5fcafcc81
KST PC <kyawcthumoeoo@gmail.com>
2022-03-27 08:43:27 +0630

    Added: Share function to dish details

--- app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/kyawsithu/cookbook/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,13 @@
 package com.kyawsithu.cookbook.view.fragments
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
@@ -21,6 +22,8 @@ import com.bumptech.glide.request.target.Target
 import com.kyawsithu.cookbook.R
 import com.kyawsithu.cookbook.application.CookBookApplication
 import com.kyawsithu.cookbook.databinding.FragmentDishDetailsBinding
+import com.kyawsithu.cookbook.model.entities.CookBook
+import com.kyawsithu.cookbook.utils.Constants
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModel
 import com.kyawsithu.cookbook.viewmodel.CookBookViewModelFactory
 import kotlinx.coroutines.flow.combine
@@ -29,6 +32,8 @@ import java.util.*
 
 class DishDetailsFragment : Fragment()
 {
+    private var mCookBookDetails: CookBook? = null
+
     private var binding : FragmentDishDetailsBinding? = null
 
     private val mCookBookViewModel: CookBookViewModel by viewModels{
@@ -38,6 +43,58 @@ class DishDetailsFragment : Fragment()
     override fun onCreate(savedInstanceState : Bundle?)
     {
         super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu : Menu, inflater : MenuInflater)
+    {
+        inflater.inflate(R.menu.menu_share, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item : MenuItem) : Boolean
+    {
+        when(item.itemId){
+            R.id.action_share_dish -> {
+                val type = "text/plain"
+                val subject = "Check out thid dish recipe"
+                var extraText = ""
+                val shareWith = "Share with"
+
+                mCookBookDetails?.let{
+                    var image = ""
+                    if(it.imageSource == Constants.DISH_IMAGE_SOURCE_ONLINE){
+                        image = it.image
+                    }
+
+                    var cookingInstructions = ""
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                        cookingInstructions = Html.fromHtml(
+                            it.directionToCook,
+                            Html.FROM_HTML_MODE_COMPACT
+                                                           ).toString()
+                    }else{
+                        @Suppress("DEPRECATION")
+                        cookingInstructions = Html.fromHtml(it.directionToCook).toString()
+                    }
+
+                    extraText =
+                            "$image \n" +
+                            "\n Title:  ${it.title} \n\n Type: ${it.type} \n\n Category: ${it.category}" +
+                            "\n\n Ingredients: \n ${it.ingredients} \n\n Instructions To Cook: \n $cookingInstructions" +
+                            "\n\n Time required to cook the dish approx ${it.cookingTime} minutes."
+                }
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = type
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                intent.putExtra(Intent.EXTRA_TEXT, extraText)
+                startActivity(Intent.createChooser(intent, shareWith))
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?,
@@ -53,6 +110,7 @@ class DishDetailsFragment : Fragment()
         super.onViewCreated(view, savedInstanceState)
         val args : DishDetailsFragmentArgs by navArgs()
 
+        mCookBookDetails = args.dishDetails
 
         args.let {
             try
@@ -101,6 +159,7 @@ class DishDetailsFragment : Fragment()
             binding !!.tvCategory.text = it.dishDetails.category
             binding !!.tvIngredients.text = it.dishDetails.ingredients
             binding !!.tvCookingDirection.text = it.dishDetails.directionToCook
+
             binding !!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time,
                 it.dishDetails.cookingTime)
 
